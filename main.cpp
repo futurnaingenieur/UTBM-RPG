2af90ce237431e4736ed7ca1d531fc9193d7ef0b
futurnaingenieur <futurnaingenieur@gmail.com>
2015-04-01 01:23:37 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,30 +1,12 @@
-#include <iostream>
-#include <SFML/Graphics.hpp>
-
-using namespace sf;
+#include "game.hpp"
 
 int main(int argc,char**argv)
 {
-    RenderWindow window(VideoMode(1200,800,32),"UTBM RPG");
-    window.setFramerateLimit(60);
+    Game game;
+    game.launch();
+    return 0;
+}
 
-    Event event;
-
-    while(window.isOpen())
-    {
-        window.clear();
-
-        while(window.pollEvent(event))
-        {
-            switch(event.type)
-            {
-            case Event::Closed:
-                window.close();
-                break;
-            default:
-                break;
-            }
-        }
         window.display();
     }
 }
